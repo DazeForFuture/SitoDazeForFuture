a728f0daef643c660965850b0dbac26fcf5da8e2
Michele Antonio Portulano <michele.antonio.portulano@gmail.com>
2025-09-11 16:03:11 +0200

    Update server.py

--- Backend/server.py
+++ Backend/server.py
@@ -1,19 +1,18 @@
 from flask import Flask, send_from_directory
 import os
 
-# Percorso assoluto alla cartella frontend
 frontend_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../frontend'))
 app = Flask(__name__, static_folder=None)
 
 @app.route('/')
 def index():
-    # Serve index.html dalla cartella frontend
+    
     return send_from_directory(frontend_dir, 'index.html')
 
 @app.route('/<path:filename>')
 def serve_page(filename):
-    # Serve qualsiasi altro file HTML dalla cartella frontend
+    
     return send_from_directory(frontend_dir, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
